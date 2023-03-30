d9a64ab35e15e99d6488c2ce922f6003e178f8da
burin2301 <burin2301@gmail.com>
2023-03-29 19:26:16 -0500

    added assets and icons, working on js logic

---
+++ script.js
@@ -0,0 +1,14 @@
+function startGame(){
+    let btnPetPlayer = document.getElementById(`btn-choose-pet`)
+    btnPetPlayer.addEventListener(`click`, choosePetPlayer)
+
+}
+startGame()
+function choosePetPlayer(){
+    //alert(`you just got the pet you elected`)
+
+    if()
+}
+
+
+
